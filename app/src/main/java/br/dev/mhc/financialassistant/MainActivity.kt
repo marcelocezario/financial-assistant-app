7ff2ce9e9d5edc9ef6207346b3d34d30484d6f59
Marcelo Cezário <marcelocezario@gmail.com>
2021-03-15 17:36:52 -0300

    Initial commit

---
+++ app/src/main/java/br/dev/mhc/financialassistant/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.dev.mhc.financialassistant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
